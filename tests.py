1518e15afe0f11cc75481b7da3572e7fcaa0a5a4
J3T21 <jameset701@gmail.com>
2024-01-23 18:32:48 +0000

    python trig func implemented

--- tests.py
+++ tests.py
@@ -20,12 +20,17 @@ def sumVector(lst):
         y += x_i + np.power(x_i, np.float32(2))
     return y
 
+def trigSum (list):
+    y=np.float32(0)
+    for x_i in list:
+        y+= np.float32(0.5)*x_i + (x_i * x_i)*np.cos((x_i-np.float32(128))/np.float32(128))
+    return y
 
 def main():
     for i in range(0, len(steps)):
         print(f"Test {i + 1}")
         test_vector = generateVector(Ns[i], steps[i])
-        y = sumVector(test_vector)
+        y = trigSum(test_vector)
         # y /= 1024
         print(f"Step: {steps[i]}, N: {Ns[i]}, Result: {y}")
         i = y.view("int32")
