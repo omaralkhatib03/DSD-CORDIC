afe0e1d356984fdad2bcebad1e9653b1e86251ca
J3T21 <jameset701@gmail.com>
2024-01-23 20:06:00 +0000

    removed multiplier support for task 5

--- hardware/software/hello_world_bsp/public.mk
+++ hardware/software/hello_world_bsp/public.mk
@@ -126,12 +126,12 @@ ELF_PATCH_FLAG  += --cpu_name $(CPU_NAME)
 ALT_CFLAGS += -mno-hw-div
 
 # Hardware Multiplier present. 
-# setting HARDWARE_MULTIPLY is true
-ALT_CFLAGS += -mhw-mul
+# setting HARDWARE_MULTIPLY is false
+ALT_CFLAGS += -mno-hw-mul
 
 # Hardware Mulx present. 
-# setting HARDWARE_MULX is true
-ALT_CFLAGS += -mhw-mulx
+# setting HARDWARE_MULX is false
+ALT_CFLAGS += -mno-hw-mulx
 
 # Debug Core present. 
 # setting HAS_DEBUG_CORE is true
@@ -161,9 +161,9 @@ SOPC_SYSID_FLAG += --sidp=0x1001030
 ELF_PATCH_FLAG  += --sidp 0x1001030
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1706037416
-SOPC_SYSID_FLAG += --timestamp=1706037416
-ELF_PATCH_FLAG  += --timestamp 1706037416
+# setting SOPC_TIMESTAMP is 1706039609
+SOPC_SYSID_FLAG += --timestamp=1706039609
+ELF_PATCH_FLAG  += --timestamp 1706039609
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
