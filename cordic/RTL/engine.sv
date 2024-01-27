d66bbacef7d67d3597f77b083898ba731a86ccde
SsagatroNN <oa321@ic.ac.uk>
2024-01-27 18:09:57 +0000

    oopsie

--- cordic/RTL/engine.sv
+++ cordic/RTL/engine.sv
@@ -9,9 +9,8 @@ module engine (
     output signed [31:0] y_n, 
     output [31:0] w_n
 );
-    // w_i[31] ? (w_i >= theta) :
-    wire sign_w = (w_i <= theta); // high is w_i is negative
-
+    wire sign_w = w_i[31] ? (w_i >= theta) : (w_i <= theta); // high is w_i is negative
+    
     assign x_n = sign_w ? x_i - (y_i >>> i) : x_i + (y_i >>> i);
     assign y_n = sign_w ? y_i + (x_i >>> i) : y_i - (x_i >>> i);
     assign w_n = sign_w ? w_i + a_i : w_i - a_i; 
