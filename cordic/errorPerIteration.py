c9ebaa850296ef56087b470ace8a5c72577e113d
SsagatroNN <oa321@ic.ac.uk>
2024-02-01 21:40:45 +0000

    no mamez weh, the guy designed the cw thats acc crazy

--- cordic/errorPerIteration.py
+++ cordic/errorPerIteration.py
@@ -4,47 +4,61 @@ import sys
 import subprocess as sp
 
 def main():
-    mean_errors = []
+    errors = []
+    confidences = []
     points = []
     minimum_iterations = 32 
     error_at_minimum = 0.0
+    confidence_at_minimum = 0.0
+    stdDiv_at_minimum = 0.0
     
     target_error = 0.5 * (10 ** -6)
+    target_confidence = 0.95
     found_minimum = 0
      
     for i in range(10, 33):
     
         sp.run([f'./scripts/setIterations.sh {i}'], shell=True) 
+        
+        cmdResult = sp.Popen("make iterations", shell=True, stdout=sp.PIPE)
 
-        tmp = [] 
-        for _ in range(0, 5): 
-            cmdResult = sp.Popen("make iterations", shell=True, stdout=sp.PIPE)
 
-            mean_error = 0    
-            line_number = 0 
-            for line in cmdResult.stdout: 
-                line_number+=1
-                if line_number == 3:
-                  tmp.append(abs(float(line))) 
-            
+        error = 1000
+        confidence = 0
+        stdDiv = 0
+        line_number = 0 
+        for line in cmdResult.stdout: 
+            line_number+=1
+            if line_number == 3:
+                string_line = str(line)
+                string_line = string_line[2:-3]
+                # print(string_line)
+                mean, std, conf = string_line.split(',') 
+                error = abs(float(mean))
+                errors.append(error)
+                confidence = float(conf)
+                confidences.append(confidence) 
+                stdDiv = float(std)
+                
+                
         points.append(i)
-
-        snd_avg = np.mean(np.array(tmp))
-        mean_errors.append(abs(snd_avg))
         
-        
-        if abs(snd_avg) < target_error and not found_minimum:
+        if error < target_error and confidence >= target_confidence and not found_minimum:
             minimum_iterations = i
-            error_at_minimum = snd_avg
+            error_at_minimum = error
+            confidence_at_minimum = confidence
+            stdDiv_at_minimum = stdDiv
+            
             found_minimum = 1
-        
-        tmp = []
 
-
-    # print(mean_errors)
-    print(f'Minimum Iterations: {minimum_iterations}, Error At Minimum: {error_at_minimum}')
+    # print(errors)
+    print(f'Minimum Iterations: {minimum_iterations}, Error At Minimum: {error_at_minimum}, Confidence At Minimum: {confidence_at_minimum}, Standard Deviation at Minimum: {stdDiv_at_minimum}')
     
-    plt.plot(points, mean_errors)
+    plt.plot(points, errors)
+    plt.axhline(y = error_at_minimum + stdDiv, color='r', linestyle='--', linewidth=0.5)
+    plt.axhline(y = error_at_minimum - stdDiv, color='r', linestyle='--', linewidth=0.5)
+    plt.axvline(x = minimum_iterations, color='g', linestyle='--', linewidth=0.5)
+    
     plt.xlabel('Number of Iterations')
     plt.ylabel('Mean Error (Monte Carlo)')
     plt.title('Mean Error Vs Number Of Iterations')
--- cordic/scripts/monteCarloSimulation.py
+++ cordic/scripts/monteCarloSimulation.py
@@ -15,7 +15,6 @@ def mean_confidence_interval(data, confidence=0.95):
 def main():
     cordic_angles = []
     theta = []
-    cosines = [] 
     
     cnt = 0;
     for line in sys.stdin:
@@ -32,9 +31,9 @@ def main():
             case r'input':
                 theta.append(np.float32(value))
 
-            case r'np\.cos':
-                cosines.append(np.float32(value))
-                
+            # case r'np\.cos':
+            #     cosines.append(np.float32(value))
+                 
             case _:
                 continue 
 
@@ -42,30 +41,35 @@ def main():
     # print(cordic_angles)
     
     assert len(theta) == len(cordic_angles) 
-    assert len(cosines) == len(cordic_angles)
     
     
     cordic_angles = np.array(cordic_angles)
-    cosines = np.array(cosines)
+    cosines = np.array([np.cos(angle) for angle in theta])
+    assert len(cosines) == len(cordic_angles)
+    
     error = cordic_angles - cosines
     mean_error = np.mean(error)
     stdDiv = np.std(error)
-    m, lower_bound, upper_bound = mean_confidence_interval(error)
-   
+    _, lower_bound, upper_bound = mean_confidence_interval(error)
+    
+    confidence = np.sum([1 for i in error if (i > (mean_error - 2 * stdDiv) and (i < mean_error + 2 * stdDiv))]) / (cnt + 1)
+
+    
+    
     # for i in range(0, len(cordic_angles)):
-    #     print(f'cos({theta[i]}) = {cosines[i]}, np.cos = {np.cos(theta[i])}, apx = {cordic_angles[i]}, exact = {cosines[i] == cordic_angles[i]}')
-    #
-    #
-    # print(f'N:{cnt}, Mean Error: {mean_error}, stdDiv: {stdDiv}, confidence_interval:{lower_bound} - {upper_bound}')  
-    print(f'{mean_error}')
-   #  print()
-   # 
+        # print(f'cos({theta[i]}) = {cosines[i]}, np.cos = {np.cos(theta[i])}, apx = {cordic_angles[i]}, exact = {cosines[i] == cordic_angles[i]}')
+    
+
+    # print(f'N:{cnt}, Mean Error: {mean_error}, stdDiv: {stdDiv}, confidence:{confidence}')  
+   
+    print(f'{mean_error}, {stdDiv}, {confidence}')
+   
     # sns.set(style="whitegrid")  # Set the style of the plot
     # sns.kdeplot(error, fill=True, color="skyblue")  # Kernel Density Estimation plot
     # plt.xlabel('Error')
     # plt.ylabel('Density')
     # plt.title('Distribution of Error')
     # plt.show()  
-   #
+   
 if __name__ == "__main__":
     main()
