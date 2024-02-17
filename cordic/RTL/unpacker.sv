063e7bc4f9b5fcd77c2a8b069a9b8ae38ab2833e
SsagatroNN <oa321@ic.ac.uk>
2024-02-17 20:48:41 +0000

    Changed data path to 26 bits

--- cordic/RTL/unpacker.sv
+++ cordic/RTL/unpacker.sv
@@ -1,20 +1,21 @@
 module unpacker #(
-    parameter FRACTIONAL_BITS = 30,
+    parameter FRACTIONAL_BITS = 24,
     parameter SIGNED = 0 // no need to be able to unpack negative values
 )(
     input [31:0] data,
-    output [31:0] result
+    output [25:0] result
 );
 
 
     wire [7:0] e;
     wire [5:0] shift;
     wire [7:0] E = data[30:23];
-    wire [31:0] concated = {SIGNED && data[31], 1'b1, data[22:0], {FRACTIONAL_BITS - 23{1'b0}}};
-
+    wire [25:0] concated = {SIGNED && data[31], 1'b1, data[22:0], {FRACTIONAL_BITS - 23{1'b0}}};
+    
+    // TODO: Implement GRS Rounding
     assign e = -E + 8'd127; 
     assign shift = E > (127 - FRACTIONAL_BITS) ? e[5:0] : FRACTIONAL_BITS;
-    assign result = data == 0 ? 32'b0 : concated >> (shift);
+    assign result = data == 0 ? 32'b0 : concated >> shift;
 
 endmodule
 
