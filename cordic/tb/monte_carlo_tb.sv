c37596c695642392f0927c5c7071327325b70ba7
SsagatroNN <oa321@ic.ac.uk>
2024-02-17 20:17:07 +0000

    changed to 26 bit data path, changed cosine output to type float for interperter

--- cordic/tb/monte_carlo_tb.sv
+++ cordic/tb/monte_carlo_tb.sv
@@ -2,12 +2,14 @@
 
 
 module tb();
+    
+    parameter WIDTH = 24;
 
     reg [31:0] angle;
     wire [31:0] result;
-    wire [31:0] theta;
-    wire [31:0] x_s [31:0]; 
-    wire [31:0] w_s [31:0]; 
+    wire [WIDTH+1:0] theta;
+    wire [WIDTH+1:0] x_s [31:0]; 
+    wire [WIDTH+1:0] w_s [31:0]; 
     reg status;
 
     cosine dut(angle, result, theta, x_s, w_s);
@@ -25,20 +27,19 @@ module tb();
         for (i = 0; i < N; i = i + 1) begin 
           _ = $fscanf('h8000_0000, "%d", angle);
           #1
-          $display("input:fl:%h,", angle, "cos-cordic:anc-30-s:%h,", result, "theta:fi-30-s:%h", theta);
+          $display("input:fl:%h,", angle, "cos-cordic:fl:%h,", result, "theta:fi-24-s:%h", theta);
         end
        // printIterations();
         
     end
 
 
-    task printIterations;    
+  task printIterations;    
       int i;
       for (i = 0; i < 2; i = i + 1) begin
-        $display("w_%0d",i,":fi-30-s:%h,", w_s[i], "x_%0d",i,":anc-30-s:%h", x_s[i]);
+        $display("w_%0d",i,":fi-24-s:%h,", w_s[i], "x_%0d",i,":anc-24-s:%h", x_s[i]);
       end 
     endtask: printIterations
-
 endmodule
 
 
