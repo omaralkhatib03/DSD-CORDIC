f4b664aa806e5b7a016e3b2198c0b305f38f2342
SsagatroNN <oa321@ic.ac.uk>
2024-02-17 20:16:00 +0000

    Added packer, changed tot 26 bit data path

--- cordic/RTL/cosine.sv
+++ cordic/RTL/cosine.sv
@@ -1,51 +1,48 @@
-module cosine (
+module cosine #(
+  parameter WIDTH = 24
+)
+(
     input [31:0] angle,
     output [31:0] result,
-    output [31:0] theta,
-    output [31:0] x_s_out [31:0],
-    output [31:0] w_s_out [31:0]
+    output [WIDTH+1:0] theta,
+    output [WIDTH+1:0] x_s_out [31:0],
+    output [WIDTH+1:0] w_s_out [31:0]
 );
 
 // unpacker: converts from floating point to fixed point
-wire [31:0] fixedFractionalAngle;
+wire [WIDTH+1:0] fixedFractionalAngle;
 unpacker upckr(angle, fixedFractionalAngle);
 
 // rom
-reg [31:0] angles [0:31];
+reg [WIDTH+1:0] angles [0:31];
 
 initial begin // with angles
     $readmemh("mem.hex", angles); 
 end
 
-    wire [31:0] x_s [0:31];
-    wire [31:0] y_s [0:31];
-    wire [31:0] w_s [0:31];
+    wire [WIDTH+1:0] x_s [0:31];
+    wire [WIDTH+1:0] y_s [0:31];
+    wire [WIDTH+1:0] w_s [0:31];
 
-// 26dd38b0 30-u
-// 4dba7700 31-u
-// 26dd3b80
-engine en0(5'b0, angles[0], 32'h26dd3b80, 32'h0, 32'h0, fixedFractionalAngle, x_s[0], y_s[0], w_s[0]);
+engine en0(5'b0, angles[0], 26'h9b74ee, 26'h0, 26'h0, fixedFractionalAngle, x_s[0], y_s[0], w_s[0]);
 
 genvar i;
 
 generate
-    for (i = 5'd1; i < 6'd32; i = i + 1) begin : gen_cordic_engines
+    for (i = 5'd1; i < 6'd19; i = i + 1) begin : gen_cordic_engines
         wire[4:0] iter = i;
         engine en(iter, angles[i], x_s[i-1], y_s[i-1], w_s[i-1], fixedFractionalAngle, x_s[i], y_s[i], w_s[i]);
     end
     
 endgenerate
-  
-assign result = x_s[31];
-assign theta = w_s[31];
+
+
+
+packer pckr(x_s[18], result);
+// assign result = x_s[18]; 
+assign theta = w_s[18];
 assign x_s_out = x_s;
 assign w_s_out = w_s;
-
-
-
-// make packer
-
-
 
 
 endmodule
