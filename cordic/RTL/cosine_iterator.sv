994db8f332c6f1e572845253a5088cc68be9c12e
SsagatroNN <oa321@ic.ac.uk>
2024-02-18 14:00:53 +0000

    Changed ROM to localparam such that quartus elaborates parameters on design

--- cordic/RTL/cosine_iterator.sv
+++ cordic/RTL/cosine_iterator.sv
@@ -9,36 +9,26 @@ module cosine #(
     output [WIDTH+1:0] w_s_out [31:0]
 );
 
-// unpacker: converts from floating point to fixed point
 wire [WIDTH+1:0] fixedFractionalAngle;
 unpacker upckr(angle, fixedFractionalAngle);
 
-// rom
-reg [WIDTH+1:0] angles [0:31];
+localparam [(WIDTH+2)*32:0] angles = {26'h0, 26'h1, 26'h1, 26'h1, 26'h1, 26'h1, 26'h1, 26'h1, 26'h1, 26'h3, 26'h8, 26'h10, 26'h1f, 26'h40, 26'h7f, 26'h100, 26'h200, 26'h3ff, 26'h7ff, 26'h1000, 26'h1fff, 26'h3fff, 26'h7fff, 26'hffff, 26'h1fffd, 26'h3ffea, 26'h7ff55, 26'hffaad, 26'h1fd5ba, 26'h3eb6ec, 26'h76b19c,26'hc90fdb};
 
-initial begin // with angles
-    $readmemh("mem.hex", angles); 
-end
 
-    wire [WIDTH+1:0] x_s [0:31];
-    wire [WIDTH+1:0] y_s [0:31];
-    wire [WIDTH+1:0] w_s [0:31];
+wire [WIDTH+1:0] x_s [0:31];
+wire [WIDTH+1:0] y_s [0:31];
+wire [WIDTH+1:0] w_s [0:31];
 
-// 26dd38b0 30-u
-// 4dba7700 31-u
-// 26dd3b80
-engine en0(5'b0, angles[0], 26'h9b74ee, 26'h0, 26'h0, fixedFractionalAngle, x_s[0], y_s[0], w_s[0]);
+engine en0(5'b0, angles[0+:26], 26'h9b74ee, 26'h0, 26'h0, fixedFractionalAngle, x_s[0], y_s[0], w_s[0]);
 
 genvar i;
 
 generate
     for (i = 5'd1; i < 6'd24; i = i + 1) begin : gen_cordic_engines
         wire[4:0] iter = i;
-        engine en(iter, angles[i], x_s[i-1], y_s[i-1], w_s[i-1], fixedFractionalAngle, x_s[i], y_s[i], w_s[i]);
+        engine en(iter, angles[(i*26)+:26], x_s[i-1], y_s[i-1], w_s[i-1], fixedFractionalAngle, x_s[i], y_s[i], w_s[i]);
     end
-    
 endgenerate
-
 
 packer pckr(x_s[23], result);
 
@@ -46,6 +36,5 @@ assign theta = w_s[23];
 assign x_s_out = x_s;
 assign w_s_out = w_s;
 
-// make packer
 
 endmodule
