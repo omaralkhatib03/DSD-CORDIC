a7687758802caa91cf999149e673dd6dfb7e759f
omaralkhatib03 <oa321@ic.ac.uk>
2024-03-01 14:26:42 +0000

    Working cos

--- hardware/custom/cordic/tb/cosine_tb.v
+++ hardware/custom/cordic/tb/cosine_tb.v
@@ -3,75 +3,77 @@
 
 module tb();
     
+    parameter WIDTH = 22;
 
     reg [31:0] angle;
     wire [31:0] result;
-  
-    reg clk, clk_en, reset;
+    wire [WIDTH+1:0] theta;
+    wire [WIDTH+1:0] x_s [31:0]; 
+    wire [WIDTH+1:0] w_s [31:0]; 
 
-    initial
-      clk = 1'b1;
-
-    always 
-      #1 clk = ~clk;
-
-    cosine dut(clk, reset, clk_en, angle, result);
+    cosine dut(angle, result, theta, x_s, w_s);
      
     initial begin
-        reset = 1'b0;
-        clk_en = 1'b1;
-
         $dumpfile("sim/cosine.vcd");
         $dumpvars();
         
         angle = 32'h3f800000; // 1
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
 
         // angle = 32'hbf800000; // -1 Illegal test case, input range [0, 255]
-        // #10
+        // #1
         // $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
 
         angle = 32'h33800000; // 2^-30, smallest value we can represent
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
 
         angle = 32'h0;  
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
        
         angle = 32'h3f000000; // 0.5
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
         
         angle = 32'h3f1b74ee;
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
         
         angle  = 32'h3f0a9594;
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
 
         angle = 32'h437f0000;
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
  
         angle = 32'h43000000;
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
  
         angle = 32'h42c80000;
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
         
         angle = 32'h43400000;
-        #10
+        #1
         $display("x:fl:%h,", angle, "angle:x:%h,", angle, "result:fl:%h", result);
 
         $finish;
 
        angle = 32'hZ;
     end
+
+
+    task printIterations;    
+      int i;
+      for (i = 0; i < 10; i = i + 1) begin
+        $display("w_%0d",i,":fi-23-s:%h,", w_s[i], "x_%0d",i,":anc-23-s:%h", x_s[i]);
+      end 
+    endtask: printIterations
+
 
 endmodule
 
