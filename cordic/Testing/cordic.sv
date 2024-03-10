bc8fbec77bb6b4c8ce996c6d9fc2712e24aba9d3
james taylor <jameset701@gmail.com>
2024-03-10 01:25:56 +0000

    started

--- cordic/Testing/cordic.sv
+++ cordic/Testing/cordic.sv
@@ -2,54 +2,166 @@ module cordic #(
     parameter FRACS = 21,
     parameter INTS = 1,
     parameter WIDTH = INTS + FRACS + 1,  //sign bit needed for algorithm
-    parameter Iterations = 15
+    parameter Iterations = 15,
+    parameter Stages = 3,
+    parameter ItersPerStage = 5 //Iterations / Stages
 )
 (
     input clk,
     input reset,
     input clk_en,
+    input start,
     input [WIDTH-1:0] fixedPoint_theta,
+    output done,
     output logic [WIDTH-2:0] fixedPoint_result //remember to drop sign bit always positive in range 0.5403 to 1 before returning
 );
 
 localparam  [WIDTH-1:0] x_start = 23'b00100110110111010011101;//0.607252935008881256169446752827757841587066381156807;
 localparam  [WIDTH-1:0] y_start = 23'b0;//0.0;
-localparam [WIDTH-2:0] tan_terms [0:Iterations-1] = {22'b0110010010000111111011, 22'b0011101101011000110011, 22'b0001111101011011011101, 22'b0000111111101010110111,
-22'b0000011111111101010101, 22'b0000001111111111101010, 22'b0000000111111111111101, 22'b0000000011111111111111, 22'b0000000001111111111111, 
-22'b0000000000111111111111, 22'b0000000000011111111111, 22'b0000000000001111111111, 22'b0000000000000111111111, 22'b0000000000000011111111, 22'b0000000000000001111111};
+localparam [WIDTH-2:0] tan_terms [0:Iterations-1] = {22'b0000000000000000111111, 22'b0000000000000001111111, 22'b0000000000000011111111, 22'b0000000000000111111111,
+22'b0000000000011111111111, 22'b0000000000111111111111, 22'b0000000001111111111111, 22'b0000000011111111111101, 22'b0000000111111111111101, 
+22'b0000001111111111101010, 22'b0000011111111111101010, 22'b0000111111101010110111, 22'b0001111101011011011101, 22'b0011101101011000110011, 22'b0110010010000111111011};
 //unsigned tan constants
 
-logic [WIDTH-1:0] z_0 ; //concatenated sign bit needed for algorithm in top module
-logic [WIDTH-1:0] x_0;
-logic [WIDTH-1:0] y_0;
-logic [WIDTH-1:0] x_1;
-logic [WIDTH-1:0] y_1;
-logic [WIDTH-1:0] z_1;
-wire di = z_0[WIDTH-1]; //high is negative 
+wire [WIDTH-1:0] x_s [ItersPerStage:0]; 
+wire [WIDTH-1:0] y_s [ItersPerStage:0];
+wire [WIDTH-1:0] z_s [ItersPerStage:0];
 
-always_ff @(posedge clk) begin
-    if (clk_en) begin
-        z_0 <= fixedPoint_theta;
-        x_0 <= x_start;
-        y_0 <= y_start;
-        for (int i = 0; i < Iterations; i++) begin
-            x_1 <= di ? x_0 + (y_0 >>> i) : x_0 - (y_0 >>> i);
-            y_1 <= di ? y_0 - (x_0 >>> i) : y_0 + (x_0 >>>i);
-            z_1 <= di ? z_0 + {1'b0, tan_terms[i]} : z_0 - {1'b0 ,tan_terms[i]};
-            x_0 <= x_1;
-            y_0 <= y_1;
-            z_0 <= z_1;
-            if (i == Iterations-1) begin
-                fixedPoint_result <= x_1;
-            end
-        end 
+reg [WIDTH-1:0] x_reg;
+reg [WIDTH-1:0] y_reg;
+reg [WIDTH-1:0] z_reg;
+
+reg [4:0] loops = 5'b0;
+
+// assign x_s[0] = x_start;
+// assign y_s[0] = y_start;
+// assign z_s[0] = fixedPoint_theta;
+
+
+
+typedef enum {IDLE, S1} stage_control;
+stage_control current_stage, next_stage;
+
+
+ always_ff@(posedge clk) begin
+    if (reset || ~clk_en) begin
+        current_stage <= IDLE;
     end
-    if (reset) begin
-        x_0 <= x_start;
-        y_0 <= y_start;
-        z_0 <= {fixedPoint_theta};
-    end  
+    else if (clk_en) begin
+        current_stage <= next_stage;
+    end
 end
 
+always_ff@(posedge clk) begin
+    if (reset || ~clk_en || current_stage == IDLE) begin
+        loops <= 5'b0;
+    end
+    else if (clk_en && current_stage == S1) begin
+        loops <= loops+1;
+    end
+end
+
+always_comb begin
+    casez (current_stage)
+        IDLE: begin
+            casez(start)
+                1'b1: next_stage = S1;
+                default: next_stage = IDLE;
+            endcase
+        end
+        S1: begin
+            next_stage = loops >= Stages ? IDLE : S1;
+        end
+        default: begin
+            next_stage = IDLE;
+        end
+    endcase
+end
+
+assign x_s[0] = start ? x_start : x_reg;
+assign y_s[0] = start ? y_start : y_reg;
+assign z_s[0] = start ? fixedPoint_theta : z_reg;
+
+always_ff@(posedge clk) begin
+    if (reset || ~clk_en) begin
+        x_reg <= {WIDTH{1'b0}};
+        y_reg <= {WIDTH{1'b0}};
+        z_reg <= {WIDTH{1'b0}};
+    end
+    else if (clk_en) begin
+        x_reg <= x_s[ItersPerStage];
+        y_reg <= y_s[ItersPerStage];
+        z_reg <= z_s[ItersPerStage];
+    end
+end
+
+genvar i;
+
+generate
+    for (i = 0; i < ItersPerStage; i++) begin: stage_gen //instantiate 5 iterations
+    wire [4:0] index = loops*ItersPerStage + i;
+    iteration iter(
+        .i(index),
+        .atan_i(tan_terms[index]),
+        .x_i(x_s[i]),
+        .y_i(y_s[i]),
+        .z_i(z_s[i]),
+        .x_n(x_s[i+1]),
+        .y_n(y_s[i+1]),
+        .z_n(z_s[i+1])
+    );
+    end
+endgenerate
+
+assign fixedPoint_result = x_reg;
+assign done = loops == Iterations;
+
 endmodule
+
+// genvar i;
+// generate
+//     for (i = 0; i < Cycles; i++) begin: stage_gen
+//     wire [WIDTH-1:0] x_internal [Cycles:0];
+//     wire [WIDTH-1:0] y_internal [Cycles:0];
+//     wire [WIDTH-1:0] z_internal [Cycles:0];
+//         stage #(
+//             .FRACS(FRACS),
+//             .INTS(INTS),
+//             .WIDTH(WIDTH)
+//         ) stage(//fix this 
+//             .i(i),
+//             .atan_i(tan_terms[i]),
+//             .x_i(x_s[i]),
+//             .y_i(y_s[i]),
+//             .z_i(z_s[i]),
+//             .x_n(x_s[i+1]),
+//             .y_n(y_s[i+1]),
+//             .z_n(z_s[i+1])
+//         );
+//     end
+// endgenerate
+
+// always_ff@(posedge clk) begin 
+//     if (reset) begin
+//         stage_counter <= 0;
+//     end
+//     else if (clk_en) begin
+//         if (stage_counter == 0) begin
+//             x_s[0] <= x_start;
+//             y_s[0] <= y_start;
+//             z_s[0] <= fixedPoint_theta;
+//         end
+//         else begin
+//             x_s[stage_counter] <= x_1;
+//             y_s[stage_counter] <= y_1;
+//             z_s[stage_counter] <= z_1;
+//         end
+//         stage_counter <= stage_counter + 1;
+//     end
+
+
+
+// end
+
+// endmodule
 
--- cordic/Testing/cordic_tb.sv
+++ cordic/Testing/cordic_tb.sv
@@ -1,14 +1,16 @@
-`timescale 1 ns / 100 ps
+`timescale 100 ps / 100 ps
 module cordic_tb();
 
     reg [22:0] fixedPoint;
-    reg clk,clk_en,reset;
+    reg clk,clk_en,reset,start;
     wire [21:0] fixedPoint_result;
-
+    wire done;
     cordic dut(
         .clk(clk),
         .reset(reset),
         .clk_en(clk_en),
+        .start(start),
+        .done(done),
         .fixedPoint_theta(fixedPoint),
         .fixedPoint_result(fixedPoint_result)     
     );
@@ -29,21 +31,23 @@ module cordic_tb();
 		
 		
 		// Wait 10 cycles (corresponds to timescale at the top) 
-		#100
+		#4
 		
         // Set the input try 0
-        fixedPoint <= 23'h0;
-
-		#100
+        start = 1'b1;
+        fixedPoint <= 23'b01000000000000000000000;
+        #2
+        start = 1'b0;
+		#4
         //try another 0.5405
-        fixedPoint <= 23'b00100010100101111000110;
-		//try another 0.99
-		#100
-        fixedPoint <= 23'b00111111010111000010100;
-        //try 1
-        #100
-        fixedPoint <=23'b01000000000000000000000; //dont need to worry about signed as even function
-		#100
+        // fixedPoint <= 23'b00100010100101111000110;
+		// //try another 0.99
+		// #4
+        // fixedPoint <= 23'b00111111010111000010100;
+        // //try 1
+        // #4
+        // fixedPoint <=23'b01000000000000000000000; //dont need to worry about signed as even function
+		// #4
 		$display($time, "<< Simulation Complete >>");
 		$stop;
 	end
