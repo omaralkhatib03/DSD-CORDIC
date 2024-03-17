d4c41b67d90defbe3cf3bd04ca5da7af32884805
omaralkhatib03 <oa321@ic.ac.uk>
2024-03-17 15:29:35 +0000

    Fixed point data and test added

--- hardware/software/tests.py
+++ hardware/software/tests.py
@@ -1,6 +1,8 @@
-import sys
 import numpy as np
- 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 steps = np.array(
     [5, 0.125, 1 / 1024],
     dtype=np.float32,
@@ -21,20 +23,26 @@ def sumVector(lst):
         y += x_i + np.power(x_i, np.float32(2))
     return y
 
+def term(x_i):
+    return np.float32(0.5)*x_i + (x_i * x_i)*np.cos((x_i-np.float32(128))/np.float32(128))
+
 def trigSum (list):
     y=np.float32(0)
     for x_i in list:
-        y+= np.float32(0.5)*x_i + (x_i * x_i)*np.cos((x_i-np.float32(128))/np.float32(128))
+        y+=term(x_i) 
     return y
+
+def doubleTerm(x_i):
+    return 0.5*x_i + (x_i * x_i)*np.cos((x_i-128)/128)
+
 def trigSumDouble (list):
     y=0
     for x_i in list:
-        y+= 0.5*x_i + (x_i * x_i)*np.cos((x_i-128)/128)
-    return y
+        y+= doubleTerm(x_i)
+    return y    
 
 
-def main():
-
+def test_cases():
     results = [ 920413.562500, 36123108.000000, 4621531648.000000]
 
     for i in range(0, len(steps)):
@@ -48,7 +56,56 @@ def main():
         print(f'Double result: {double}') 
         accuracy=(abs(y-double)/double) * 100 
         print(f'accuracy: {accuracy} %')
+
+
+def main():
+    # fields = ['n', 'input', 'val']
+    df = pd.read_csv('data.csv')
     
+    inputs = df['input']
+    
+    doubleTerms = []
+
+
+    for i in inputs:
+        doubleTerms.append(doubleTerm(i))
+    
+    error = doubleTerms - np.array(df['val'])
+    df.insert(3, "error", error)
+
+    print(df)
+    inputs = np.array(inputs)
+
+    plt.subplot(2, 1, 1)
+    sns.kdeplot(inputs, color='red', fill=True)
+    plt.title('PDF of Floating-Point Values')
+    plt.xlabel('Value')
+    plt.ylabel('Probability Density')
+    
+    mean_error = error.mean()
+    print(mean_error)
+    plt.subplot(2, 1, 2)
+    sns.kdeplot(error, color='blue', fill=True)
+    plt.axvline(x=mean_error, color='green', linestyle='--', label=f'Mean Error: {mean_error:.4f}')
+    plt.title('PDF of Error against Double Precision')
+    plt.xlabel('Error')
+    plt.ylabel('Probability Density')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show() 
+
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
